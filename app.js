a259b5e887c698962d5080fe36476af5c07388c7
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-15 09:55:03 -0600

    [add && update] middleware

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 
 // Middleware
 
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Routes
 
 app.get('/:username', (req, res) => {
@@ -14,8 +19,19 @@ app.get('/:username', (req, res) => {
   })
 
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-    });
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+  })
+
 // Start Server
 
 app.listen(3000, () => {
