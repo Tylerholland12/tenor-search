e7873bfe68a36a9be77028e7c26f79a9b6fd2747
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-15 09:58:07 -0600

    [add && update] search form

--- app.js
+++ app.js
@@ -19,11 +19,10 @@ app.get('/:username', (req, res) => {
   })
 
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
-})
+    console.log(req.query) // => "{ term: hey" }
+
+    res.render('home')
+  })
 
 app.get('/greetings/:name', (req, res) => {
     // grab the name from the path provided
